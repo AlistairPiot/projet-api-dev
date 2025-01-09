4ea0794321bde287b90192743fa99b2a9ea94ac3
Tilot <tilit51@gmail.com>
2025-01-09 11:21:44 +0100

    creation du 1er model

---
+++ models/Users.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+// Définir le schéma
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  fisrt_name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+
+});
+
+module.exports = mongoose.model('Users', userSchema);

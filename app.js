8617ddb3977e29785547fe3da41c895c7f97ca93
Alistair_Piot <alistair.pro15@gmail.com>
2025-01-09 11:11:53 +0100

    app.ps => configuration de la connexion (user, mdp, name db)

--- app.js
+++ app.js
@@ -2,20 +2,21 @@
 const express = require("express");
 
 // on importe le paquet mongoose
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // On crée l'application Express
 const app = express();
-
-
 
 // On export l’application
 module.exports = app;
 
 mongoose
-  .connect("mongodb+srv://<user>:<password>@cluster0.2vatb0h.mongodb.net/<database>?retryWrites=true&w=majority", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("Connexion à MongoDB réussie !"))
-  .catch(() => console.log("Connexion à MongoDB échouée !"))
+    .connect(
+        "mongodb+srv://alistairpiot:CIcHMMfHJ3NoXnXl@cluster0.s93c4.mongodb.net/db-api-restaurant?retryWrites=true&w=majority",
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }
+    )
+    .then(() => console.log("Connexion à MongoDB réussie !"))
+    .catch(() => console.log("Connexion à MongoDB échouée !"));

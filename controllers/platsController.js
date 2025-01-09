2516fc8e07136afa64ff75442630a5bb2667cab0
Tilot <tilit51@gmail.com>
2025-01-09 16:07:02 +0100

    ajout controller du plat

--- controllers/platsController.js
+++ controllers/platsController.js
@@ -1,1 +1,46 @@
-pokgepigj
+const Plats = require("../models/Plats.js");
+
+// fonction get pour récupérer tous les plats
+exports.getAllPlats = (req, res, next) => {
+    Plats.find()
+      .then((plats) => res.status(200).json(plats))
+      .catch((error) => res.status(400).json({ error }))
+  };
+
+// fonction get pour récupérer un plat
+exports.getOnePlats = (req, res, next) => {
+    Plats.findOne({ _id: req.params.id })
+      .then(plat => 
+        res.status(200).json({
+        id : plat._id,
+        nom: plat.nom,
+        description: plat.description,
+        prix : plat.prix,
+        specialite : plat.specialite, 
+    }))
+      .catch(error => res.status(404).json({ error }));
+  };
+
+// fonction post pour ajouter un plat
+exports.createPlatsById = (req, res, next) => {
+  const plat = new Plats({
+    ...req.body // On décompose toutes les données dans le req.body
+  });
+  plat.save() // On enregistre dans la BDD
+    .then(() => res.status(201).json({ message: "Le plat vient d'être créé !"}))
+    .catch(error => res.status(400).json({ error : error }));
+};
+
+// fonction delete pour supprimer un plat
+exports.deletePlatsById = (req, res, next) => {
+  Plats.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: "Plat supprimé !" }))
+    .catch((error) => res.status(400).json({ error }))
+};
+
+// fonction put pour modifier un plat
+exports.updatePlatsById = (req, res, next) => {
+  Plats.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: "Plat modifié !" }))
+    .catch((error) => res.status(400).json({ error }))
+};

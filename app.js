62f62951b3c676e32f1859c5e241f061de63ea3c
Tilot <tilit51@gmail.com>
2025-01-09 11:07:49 +0100

    connexion à la db mongo

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 // on importe le paquet express
 const express = require("express");
 
+// on importe le paquet mongoose
+const mongoose = require('mongoose');
+
 // On crée l'application Express
 const app = express();
 
+
+
 // On export l’application
 module.exports = app;
+
+mongoose
+  .connect("mongodb+srv://<user>:<password>@cluster0.2vatb0h.mongodb.net/<database>?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"))

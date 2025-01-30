006604cae3b258563133cb0f571010bf3a4b0442
Tilot <tilit51@gmail.com>
2025-01-30 16:08:22 +0100

    creation de la route pour verifier les stock bas

--- routes/stocks.js
+++ routes/stocks.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const stocksCtrl = require("../controllers/stocksController.js");
 const auth = require("./../middleware/auth.js");
-
+const Stocks = require('../models/Stocks'); // Importation du modèle Stocks
 
 
 router.get("/",auth, stocksCtrl.getAllStocks);
@@ -14,5 +14,17 @@ router.get("/:id",auth,stocksCtrl.getOneStock);
 router.put("/:id",auth, stocksCtrl.updateStockById);
 
 router.delete("/:id",auth, stocksCtrl.deleteStockById);
+
+// route pour obtenier les stocks de faible quantité
+router.get('/low-stock', (req, res) => {
+    Stocks.find({ quantity: { $lt: 500 } }, (err, lowStockItems) => {
+        if (err) {
+            res.status(500).json({ message: 'Erreur serveur', error: err });
+            return;
+        }
+        res.json(lowStockItems);
+    });
+});
+
 // // Fin du document
 module.exports = router;
